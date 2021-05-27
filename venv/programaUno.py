a1a152889fd892d9fe91f18d87ba51a6085974d5
Eduardo Echeverria <eduardoecheverria@MacBook-Pro-de-Eduardo.local>
2021-05-26 23:01:48 -0500

    First trash

---
+++ venv/programaUno.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as pit
+import pandas as pd
+
+dataset = pd.read_csv('venv/Data.csv')
+
+x = dataset.iloc[:,:-1].values #feature matrix
+y = dataset.iloc[:,-1].values #Variable dependiente
+
+print(x)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+imputer.fit(x[:,1:3]) # Identificar y transformar celdas vacias
+x[:,1:3] = imputer.transform(x[:,1:3]) # Actualiza la feature matrix con la media
+
+print(x)
+
+
+
+
