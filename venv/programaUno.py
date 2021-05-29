c8a8439258f0e6481fab8497862894674781a2f9
Eduardo Echeverria <eduardoecheverria@MacBook-Pro-de-Eduardo.local>
2021-05-28 21:31:43 -0500

    Fourth Trash

--- venv/programaUno.py
+++ venv/programaUno.py
@@ -31,5 +31,20 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 
 print(y)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state=1)
+
+print('------------ Test & Train Sets --------------')
+print('------------ Train X --------------')
+print(x_train)
+print('------------ Test X --------------')
+print(x_test)
+print('------------ Train Y --------------')
+print(y_train)
+print('------------ Test Y --------------')
+print(y_test)
+
 #Comentario prueba
 
